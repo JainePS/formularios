5613a88290b92032fa3bd44230049e3a4456062b
Jaine PS <jaine.aps@hotmail.com>
2022-04-21 12:42:34 -0300

    Obtendo a info do usuário

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
@@ -7,14 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateComponent implements OnInit {
 
+  user = {
+    name:'Jaine'
+  };
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  toHold(){
-    console.log('Submit disparado');
-    
+  toHold(form: NgForm){
+    console.log(form);
+    console.log(form.value);
+        
   }
 
 }
